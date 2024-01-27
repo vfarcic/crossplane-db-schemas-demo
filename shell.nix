94710682973f229836ca0503ec9e7b24d6340903
Viktor Farcic <viktor@farcic.com>
2024-01-27 19:00:22 +0100

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    gum
+    git
+    kubernetes-helm
+    kubectl
+    kind
+    yq-go
+    bat
+    awscli2
+    google-cloud-sdk
+    azure-cli
+  ];
+  shellHook =
+  ''
+    curl -sL "https://raw.githubusercontent.com/crossplane/crossplane/master/install.sh" | sh
+    mkdir -p bin
+    mv crossplane bin/.
+    export PATH=$PWD/bin:$PATH
+  '';
+}
